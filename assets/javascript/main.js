72fa88dd0b5f5e548fa53c087e271ad68341d425
David Potsiadlo <davidpots@gmail.com>
2017-12-27 16:08:05 -0600

    Relative url stuff

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,17 +12,17 @@ $(window).bind("load", function() {
 
       if ( $(this).hasClass('platform-link--facebook') ) {
         $('.winbox-container').css('background-color','#3C5B9B');
-        $('.winbox-container').css('background-image','url(/assets/images/winbox-facebook.svg)');
+        $('.winbox-container').css('background-image','url(assets/images/winbox-facebook.svg)');
       }
 
       if ( $(this).hasClass('platform-link--twitter') ) {
         $('.winbox-container').css('background-color','#2DAAE1');
-        $('.winbox-container').css('background-image','url(/assets/images/winbox-twitter.svg)');
+        $('.winbox-container').css('background-image','url(assets/images/winbox-twitter.svg)');
       }
 
       if ( $(this).hasClass('platform-link--yelp') ) {
         $('.winbox-container').css('background-color','#C61B00');
-        $('.winbox-container').css('background-image','url(/assets/images/winbox-yelp.svg)');
+        $('.winbox-container').css('background-image','url(assets/images/winbox-yelp.svg)');
       }
 
       return false;
