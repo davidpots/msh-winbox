ab2a7c90350c65a6f9041cf4a56ef2821dddb822
David Potsiadlo <davidpots@gmail.com>
2018-01-02 16:49:29 -0600

    Gets parameter passing working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,28 +1,66 @@
-function renderTheThing(){
-        html2canvas($("#widget"), {
-            onrendered: function(canvas) {
-                // theCanvas = canvas;
-                // document.body.appendChild(canvas);
-                // var canvasX = canvas;
-                var dataURL = canvas.toDataURL();
-                console.log(dataURL);
-                $('#downloader').attr('href',dataURL);
-                // Convert and download as image
-                // Canvas2Image.saveAsPNG(canvas);
-                // $("#img-out").append(canvas);
-                // var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-                // function convertCanvasToImage(canvas) {
-                // 	var image = new Image();
-                // 	image.src = canvas.toDataURL("image/png");
-                // 	return image;
-                // }
-                //
-                // $('body').append(convertCanvasToImage(canvas));
-                // console.log(convertCanvasToImage(canvas));
-                // Clean up
-                // document.body.removeChild(canvas);
-            }
+// Facebook = 19207
+// Twitter = 19208
+// Yelp = 19213
+
+    // fieldValues: {
+    //    'customfield_15628' : winPlatform,
+    //
+    // }
+       // summary : 'XOXOX for new website designs',
+       // 'starrating' : null
+
+// var winPlatform = "19208";
+
+        // 'customfield_16107' : '19203', // ==== Win Type : Great Interaction ====
+        // 'customfield_16107' : '19204', // ==== Win Type : New Customer via HubT ====
+        // 'customfield_16107' : '19205', // ==== Win Type : Review Upgraded ====
+
+        // 'customfield_21201' : '19200', // ==== Review Rating : 5 ====
+        // 'customfield_21201' : '19201', // ==== Review Rating : 4 ====
+        // 'customfield_21201' : '19202', // ==== Review Rating : 3 ====
+
+
+
+var winPlatform = 'customfield_15628';
+var winType = 'customfield_16107';
+var reviewUpgradedTo = 'customfield_21201';
+var fieldValues = { [winPlatform] : "19208",
+                    summary : "New Win",
+                    [winType] : "19203",
+                    [reviewUpgradedTo] : "19201" };
+
+window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
+
+    // ==== custom trigger function ====
+    triggerFunction : function( showCollectorDialog ) {
+        $('#atlwdg-trigger').on( 'click', function(e) {
+            e.preventDefault();
+            showCollectorDialog();
         });
+    }, fieldValues
+});
+
+
+// Steven's advice on how to fix this -- see the stuff in the click-event area at bottom of page
+          // function updateFieldValues(changedFields){
+          //   fieldValues = $.extend(fieldValues, changedFields)
+
+function updateFieldValues(){
+  console.log("I am inside updateFieldValues!! -- ", fieldValues);
+  window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
+    fieldValues
+  });
+}
+
+
+
+function renderTheThing(){
+  html2canvas($("#widget"), {
+    onrendered: function(canvas) {
+      var dataURL = canvas.toDataURL();
+      $('#downloader').attr('href',dataURL);
+    }
+  });
 }
 
 
@@ -34,22 +72,30 @@ function renderTheThing(){
 
 $(window).bind("load", function() {
 
+    $('.jira-creator-link').click(function(e) {
+      e.preventDefault();
+    });
+
+
     $("#btnSave").click(function(e) {
         e.preventDefault();
         renderTheThing();
         setTimeout(function() {
           $('#downloader')[0].click();
         },500);
+
+        // auto trigger the Jira button
+            // setTimeout(function() {
+            //   $('#atlwdg-trigger')[0].click();
+            // },2000);
+
     });
 
-  $('#downloader').click(function(){
-    // return false;
-  });
 
+    // Set default platform on page load
     setTimeout(function() {
-
-    $('.platform-link--facebook').trigger('click');
-        },10);
+      $('.platform-link--facebook').trigger('click');
+    },10);
 
 
     $('.platform-link').click(function(){
@@ -57,19 +103,30 @@ $(window).bind("load", function() {
       $(this).addClass('platform-link--active');
 
       if ( $(this).hasClass('platform-link--facebook') ) {
+        fieldValues[winPlatform] = "19207";
+        updateFieldValues();
         $('.winbox-container').css('background-color','#3C5B9B');
         $('.winbox-container').css('background-image','url(assets/images/winbox-facebook.svg)');
       }
 
       if ( $(this).hasClass('platform-link--twitter') ) {
+        fieldValues[winPlatform] = "19208";
+        updateFieldValues();
         $('.winbox-container').css('background-color','#2DAAE1');
         $('.winbox-container').css('background-image','url(assets/images/winbox-twitter.svg)');
       }
 
       if ( $(this).hasClass('platform-link--yelp') ) {
+        fieldValues[winPlatform] = "19213";
+        updateFieldValues();
+
+        // updateFieldValues({ [winPlatform]: "19213" })
+        // updateFieldValues({ [starRating]: 4, [winPlatform]: '19213' })
         $('.winbox-container').css('background-color','#C61B00');
         $('.winbox-container').css('background-image','url(assets/images/winbox-yelp.svg)');
       }
+
+      console.log("Win Platform set to: " + fieldValues[winPlatform]);
 
       return false;
     });
