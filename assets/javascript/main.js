9c4f271fb35b77a5350293f5d50469cf3596ba08
David Potsiadlo <davidpots@gmail.com>
2018-01-05 15:47:14 -0600

    Removes styling applying on paste which is bad but my fix is good

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -167,3 +167,16 @@ $('.picker-item').click(function(){
 
 
 });
+
+// Strip styling on paste into the text box (via https://stackoverflow.com/a/12028136)
+
+    window.addEventListener("paste", function(e) {
+        // cancel paste
+        e.preventDefault();
+
+        // get text representation of clipboard
+        var text = e.clipboardData.getData("text/plain");
+
+        // insert text manually
+        document.execCommand("insertHTML", false, text);
+    });
